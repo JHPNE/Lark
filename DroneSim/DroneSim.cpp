8d0d9af2667d7eac78b760b17bffad5c34eeb424
yeeezy <yeeezy@DESKTOP-FMU4O2J>
2024-10-24 20:31:23 +0200

    Add project files.

---
+++ DroneSim/DroneSim.cpp
@@ -0,0 +1,10 @@
+// DroneSim.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnDroneSim()
+{
+}
---
+++ DrosimEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DrosimEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
