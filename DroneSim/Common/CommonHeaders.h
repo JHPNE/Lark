4b63edacab9ddc2ada47a75693ee6f79a88e41af
JHPNE <artoox61@googlemail.com>
2024-10-28 22:16:49 +0100

    Indentifiers for Data Oriented Development

---
+++ DroneSim/Common/CommonHeaders.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// C/C++
+#include <stdint.h>
+#include <assert.h>
+#include <typeinfo>
+
+// Primitive Types
+#include "PrimitiveTypes.h"
+
+//
---
+++ DroneSim/Components/Entity.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ComponentCommon.h"
+
+namespace drosim {
+
+	#define INIT_INFO(component) namespace component { struct init_info; }
+	INIT_INFO(transform);
+	#undef INIT_INFO
+
+
+	namespace game_entity {
+		struct entityInfo {
+			transform::init_info* transform{ nullptr };
+		};
+
+		u32 create_game_entity(const entityInfo& info);
+		void remove_game_entity(entity_id id);
+		bool is_alive(entity_id id);
+	}
+
+}
+
