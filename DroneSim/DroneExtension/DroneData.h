b74e64a1f9f16ac1d6d69521877a057a2a1c43cb
JHPNE <artoox61@googlemail.com>
2025-01-08 00:09:46 +0100

    Adds Rotor Information in DroneData

--- DroneSim/DroneExtension/DroneData.h
+++ DroneSim/DroneExtension/DroneData.h
@@ -34,6 +34,16 @@ namespace lark::drone_data {
 
   struct RotorBody : Body{
     BodyType type = BodyType::ROTOR;
+    float bladeRadius = 0.f;
+    float bladePitch = 0.f;
+    unsigned int bladeCount = 1;
+    // TODO: Move to Environment
+    float airDensity = 0.f;
+    float discArea = 1.f;
+    float liftCoefficient = 0.f;
+    float currentRPM = 0.f;
+    // TODO: Visually set origin as rotation achses maybe offer user the ability to do ithimself
+    btVector3 rotorNormal{0, 0, 0};
   };
 
   struct WingBody : Body {
