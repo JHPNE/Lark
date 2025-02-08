873f8b4a7d927f04bd8ae651d326f5a0723d5e44
JHPNE <artoox61@googlemail.com>
2025-02-08 23:08:30 +0100

    Added Two Classes

---
+++ DroneSim/Physics/Controller.h
@@ -0,0 +1,8 @@
+//
+// Created by yeeezy on 08.02.2025.
+//
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#endif //CONTROLLER_H
---
+++ DroneSim/Physics/Environment.h
@@ -0,0 +1,8 @@
+//
+// Created by yeeezy on 08.02.2025.
+//
+
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#endif //ENVIRONMENT_H
