a98950481e688a32ab7ec7833b1f81e08b196130
Adnan <artoox61@googlemail.com>
2025-09-08 17:07:03 +0200

    Added LadderWind

--- Lark/PhysicExtension/Utils/Wind.h
+++ Lark/PhysicExtension/Utils/Wind.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <utility>
+#include <random>
+#include <__random/random_device.h>
 
 #include "PhysicsMath.h"
 
@@ -61,7 +62,95 @@ namespace lark::drones {
 
     class LadderWind : public Wind {
     public:
-        explicit LadderWind(Eigen::Vector3f min = Eigen::Vector3f(-1, -1, -1), Eigen::Vector3f max = Eigen::Vector3f(1, 1, 1), Eigen::Vector3f duration = Eigen::Vector3f(1, 1, 1), Eigen::Vector3f Nstep = Eigen::Vector3f(5, 5, 5,), bool random = false) {
+        explicit LadderWind(Eigen::Vector3f min = Eigen::Vector3f(-1, -1, -1),
+                            Eigen::Vector3f max = Eigen::Vector3f(1, 1, 1),
+                            Eigen::Vector3f d = Eigen::Vector3f(1, 1, 1),
+                            Eigen::Vector3f Nstep = Eigen::Vector3f(5, 5, 5),
+                            bool r = false)
+                                : duration(d), random(r), gen(std::random_device{}()) {
+            // Input validation
+            if (Nstep.x() <= 0 || Nstep.y() <= 0 || Nstep.z() <= 0) {
+                throw std::invalid_argument("LadderWind Error: The number of steps must be greater than 0");
+            }
+            // Store step counts
+            nx = static_cast<int>(Nstep.x());
+            ny = static_cast<int>(Nstep.y());
+            nz = static_cast<int>(Nstep.z());
+
+            // Create arrays
+            wx_arr = Eigen::VectorXf::LinSpaced(nx, min.x(), max.x());
+            wy_arr = Eigen::VectorXf::LinSpaced(ny, min.y(), max.y());
+            wz_arr = Eigen::VectorXf::LinSpaced(nz, min.z(), max.z());
+
+            if (random) {
+                std::uniform_int_distribution<> distx(0, nx - 1);
+                std::uniform_int_distribution<> disty(0, ny - 1);
+                std::uniform_int_distribution<> distz(0, nz - 1);
+
+                xid = distx(gen);
+                yid = disty(gen);
+                zid = distz(gen);
+            } else {
+                xid = yid = zid = 0;
+            }
+
+            timer = Vector3f(-1.f, -1.f, -1.f);
+
+            wx = wx_arr[xid];
+            wy = wy_arr[yid];
+            wz = wz_arr[zid];
         }
+
+        Eigen::Vector3f update(float t, Eigen::Vector3f position) override {
+
+            if (timer.x() == -1.f) {
+                timer = Vector3f(t, t, t);
+            }
+
+            if ((t - timer.x()) >= duration.x()) {
+                if (random) {
+                    std::uniform_int_distribution<> distx(0, nx - 1);
+                    xid = distx(gen);
+                } else {
+                    xid = (xid + 1) % nx;
+                }
+                wx = wx_arr[xid];
+                timer.x() = t;
+            }
+
+            if ((t - timer.y()) >= duration.y()) {
+                if (random) {
+                    std::uniform_int_distribution<> disty(0, ny - 1);
+                    yid = disty(gen);
+                } else {
+                    yid = (yid + 1) % ny;
+                }
+                wy = wy_arr[yid];
+                timer.y() = t;
+            }
+
+            if ((t - timer.z()) >= duration.z()) {
+                if (random) {
+                    std::uniform_int_distribution<> distz(0, nz - 1);
+                    zid = distz(gen);
+                } else {
+                    zid = (zid + 1) % nz;
+                }
+                wz = wz_arr[zid];
+                timer.z() = t;
+            }
+
+            return {wx, wy, wz};
+        }
+
+    private:
+        int xid, yid, zid;
+        int nx, ny, nz;
+        Eigen::VectorXf wx_arr, wy_arr, wz_arr;
+        float wx, wy, wz;
+        Eigen::Vector3f duration;
+        Eigen::Vector3f timer;
+        bool random;
+        std::mt19937 gen;
     };
 }
